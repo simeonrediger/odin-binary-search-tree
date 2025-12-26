61657bba40cdc83c76e96217116d6d4b5811181f
Simeon Rediger <simeonrediger@gmail.com>
2025-12-25 23:08:31 -0500

    Extract Tree array argument to nums variable

--- src/main.js
+++ src/main.js
@@ -1,5 +1,4 @@
 import Tree from './tree.js';
 
-const tree = new Tree([
-    23, 44, 61, 93, 36, 31, 77, 26, 12, 98, 74, 17, 51, 89, 52,
-]);
+const nums = [23, 44, 61, 93, 36, 31, 77, 26, 12, 98, 74, 17, 51, 89, 52];
+const tree = new Tree(nums);
