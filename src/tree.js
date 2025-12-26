4737d5abe4177110c663ed668135a3a9db7916a2
Simeon Rediger <simeonrediger@gmail.com>
2025-12-25 22:26:28 -0500

    Introduce Tree class

---
+++ src/tree.js
@@ -0,0 +1,9 @@
+class Tree {
+    constructor(sortedArray) {
+        this.root = this.build(sortedArray);
+    }
+
+    build(sortedArray, start = 0, end = sortedArray.length) {
+        // TODO
+    }
+}
